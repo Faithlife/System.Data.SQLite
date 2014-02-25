54fb30c87d9fd84fc4894417a596e8fdcdc0a886
Bradley Grainger <bgrainger@gmail.com>
2014-02-24 20:47:47 -0800

    Add build script.

---
+++ build.ps1
@@ -0,0 +1,19 @@
+Task Default -depends SourceIndex
+
+Task Build {
+  Exec { msbuild /m:4 /property:Configuration=Release System.Data.SQLite.sln }
+}
+
+Task Tests -depends Build {
+  mkdir build -force
+  Exec { tools\NUnit\nunit-console.exe /nologo /framework=4.0 /xml=build\System.Data.SQLite.xml tests\System.Data.SQLite.nunit }
+}
+
+Task SourceIndex -depends Tests {
+  $headSha = & "C:\Program Files (x86)\Git\bin\git.exe" rev-parse HEAD
+  Exec { tools\SourceIndex\github-sourceindexer.ps1 -symbolsFolder src\System.Data.SQLite\bin\Release -userId LogosBible -repository System.Data.SQLite -branch $headSha -sourcesRoot ${pwd} -dbgToolsPath "C:\Program Files (x86)\Windows Kits\8.0\Debuggers\x86" -gitHubUrl "https://raw.github.com" -serverIsRaw -verbose }
+}
+
+Task NuGetPack -depends SourceIndex {
+  Exec { nuget pack src\System.Data.SQLite\System.Data.SQLite.csproj -Prop Configuration=Release -Symbols }
+}
