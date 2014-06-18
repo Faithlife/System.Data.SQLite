52f3d1a4374426a06078bf7504ba2eca3f1cfbdb
Bradley Grainger <bgrainger@gmail.com>
2014-06-18 12:04:13 -0700

    Version 2.5.0: Support indexed parameters.

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.4.2.0")]
-[assembly: AssemblyFileVersion("2.4.2.0")]
-[assembly: AssemblyInformationalVersion("2.4.2")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.5.0")]
