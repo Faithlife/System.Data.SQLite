497faab701e2ac98ea23e70f5473b3773fcfdd82
Tom Philpot <tom.philpot@logos.com>
2015-01-21 15:12:35 -0700

    Add Xamarin iOS platform for building Xamarin.iOS projects.
    This works around AppVeyor not being able to build iOS projects, but
    allows custom CI builds to specify a configuration that exclusively
    builds the Xamarin.iOS project.

--- build.ps1
+++ build.ps1
@@ -7,6 +7,7 @@ Task Default -depends NuGetPack
 Task Build {
   Exec { nuget restore }
   Exec { msbuild /m:4 /p:Configuration=$configuration /p:Platform="Any CPU" /p:VisualStudioVersion=12.0 System.Data.SQLite.sln }
+  Exec { msbuild /m:4 /p:Configuration=$configuration /p:Platform="Xamarin iOS" /p:VisualStudioVersion=12.0 System.Data.SQLite.sln }
 }
 
 Task Tests -depends Build {
