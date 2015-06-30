c324dde4dd159b869d34420e27b6107522529b31
David Schwegler <david.schwegler@logos.com>
2015-06-30 13:26:52 -0700

    Update SolutionVersion to 2.11.1

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.11.0.0")]
-[assembly: AssemblyFileVersion("2.11.0.0")]
-[assembly: AssemblyInformationalVersion("2.11.0")]
+[assembly: AssemblyVersion("2.11.1.0")]
+[assembly: AssemblyFileVersion("2.11.1.0")]
+[assembly: AssemblyInformationalVersion("2.11.1")]
