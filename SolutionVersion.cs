fe8ff410ad8d28424c0071236a3a33431482d6c3
Dave Dunkin <dave.dunkin@logos.com>
2015-07-01 11:48:43 -0700

    Use nuget reference for Faithlife.PortableContrib. Change package prefix from Logos to Faithlife. Update version to 2.11.2.

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.11.1.0")]
-[assembly: AssemblyFileVersion("2.11.1.0")]
-[assembly: AssemblyInformationalVersion("2.11.1")]
+[assembly: AssemblyVersion("2.11.2.0")]
+[assembly: AssemblyFileVersion("2.11.2.0")]
+[assembly: AssemblyInformationalVersion("2.11.2")]
