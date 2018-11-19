fd800445daa33a9280ce15bb94f3359bffdc05e4
Bradley Grainger <bgrainger@gmail.com>
2018-11-19 12:47:55 -0800

    Update version to 2.13.0.

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.12.3.0")]
-[assembly: AssemblyFileVersion("2.12.3.0")]
-[assembly: AssemblyInformationalVersion("2.12.3")]
+[assembly: AssemblyVersion("2.13.0.0")]
+[assembly: AssemblyFileVersion("2.13.0.0")]
+[assembly: AssemblyInformationalVersion("2.13.0")]
