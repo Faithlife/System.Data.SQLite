a72c9ec5b4b8cd39da4bea9490be5274d237186e
Ed Ball <ed.ball@faithlife.com>
2019-06-04 09:21:17 -0700

    Add Xamarin test runner project.

---
+++ xamarin/XamarinTestRunner/XamarinTestRunner.iOS/AppDelegate.cs
@@ -0,0 +1,22 @@
+﻿using Foundation;
+using NUnit.Runner.Services;
+using UIKit;
+
+namespace XamarinTestRunner.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+			var nunit = new NUnit.Runner.App();
+			nunit.AddTestAssembly(typeof(System.Data.SQLite.Tests.PlatformTests).Assembly);
+			nunit.Options = new TestOptions { AutoRun = true };
+			LoadApplication(nunit);
+
+			return base.FinishedLaunching(app, options);
+        }
+    }
+}
